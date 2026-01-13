df4565441fbb5ab05602ba3782e812d84ec00165
luxondev <andreslauson@gmail.com>
2026-01-12 21:09:07 -0700

    feature layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+//STYLE SHEET FOR HEADER COMPONENT
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+        <h1 className="header__title">My Portfolio Header</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+//STYLE SHEET FOR MAIN COMPONENT
+import './Main.css'
+
+
+function Main() {
+  return (
+    <main className="main">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the main content area.</p>
+    </main>
+  )
+}
+
+export default Main 

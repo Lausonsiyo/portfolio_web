fbf6491e5321a81f53d38072c27fbb280d74e83e
luxondev <andreslauson@gmail.com>
2026-02-26 23:25:45 -0700

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 //STYLE SHEET FOR HEADER COMPONENT
-import './Header.css';
+import "./Header.css";
 
 //COMPONENT IMPORTS
-import Navbar from '../Navbar/Navbar'; 
+import Navbar from "../Navbar/Navbar";
 
 function Header() {
   return (
     <header className="header">
-        <h1 className="header__title">My Portfolio Header</h1>
-        <Navbar/>
+      <h1 className="header__title">My Portfolio Header</h1>
+      <h1 className="header__title">My Portfolio Header</h1>
+      <h1 className="header__title">My Portfolio Header</h1>
+      <Navbar />
     </header>
   );
 }

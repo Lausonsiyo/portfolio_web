ee6ef5494cc7e9fb9ea3e439cface96df17f227c
luxondev <andreslauson@gmail.com>
2026-03-09 21:01:11 -0600

    fix nav and main

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,38 @@
 //STYLE SHEET FOR NAVBAR COMPONENT
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar__list">
-        <li className="navbar__item"><a href="#home" className="navbar__link">Home</a></li>
-        <li className="navbar__item"><a href="#about" className="navbar__link">About</a></li>
-        <li className="navbar__item"><a href="#projects" className="navbar__link">Projects</a></li>
-        <li className="navbar__item"><a href="#contact" className="navbar__link">Contact</a></li>
+        <li className="navbar__item">
+          <a href="#home" className="navbar__link">
+            Home
+          </a>
+        </li>
+        <li className="navbar__item">
+          <a href="#about" className="navbar__link">
+            About
+          </a>
+        </li>
+        <li className="navbar__item">
+          <a href="#projects" className="navbar__link">
+            Projects
+          </a>
+        </li>
+        <li className="navbar__item">
+          <a href="#contact" className="navbar__link">
+            Contact
+          </a>
+        </li>
+        <li className="navbar__item">
+          <a href="#contact" className="navbar__link">
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar;

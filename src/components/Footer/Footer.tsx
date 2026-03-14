080b52e578f8f7afd2ae92add6bb33f589d372b4
luxondev <andreslauson@gmail.com>
2026-03-14 15:14:45 -0600

    feature footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,8 @@ function Footer() {
   return (
     <footer className="footer">
       <p>© 2026 My Portfolio. All rights reserved.</p>
+      <p>© 2026 My Portfolio. All rights reserved.</p>
+      <p>© 2026 My Portfolio. All rights reserved.</p>
     </footer>
   );
 }
